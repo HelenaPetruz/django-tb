a719b8aa560defff29ef04f3882de7921be4e1b7
HelenaPetruz <helena.petruz@gmail.com>
2025-05-16 18:43:03 -0300

    tela de treinar

--- tb_project/tb_app/views.py
+++ tb_project/tb_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 from .models import Exercicios, Treino, NivelDificuldade, MusculosEnvolvidos, RelExerciciosMusculos, Plano, RelTreinoExercicio
 
@@ -76,13 +76,55 @@ def treino(request, pk):
 def treinoia(request,pk):
     treino = Treino.objects.get(id_treino=pk)
     rel_treino_exercicio = RelTreinoExercicio.objects.filter(id_treino=pk)
-    exercicios = Exercicios.objects.filter(id_exercicios__in=[rel.id_exercicio for rel in rel_treino_exercicio])
+    exercicios = list(Exercicios.objects.filter(id_exercicios__in=[rel.id_exercicio for rel in rel_treino_exercicio]))
+
+    if 'indice' not in request.session or 'serie_atual' not in request.session:
+        request.session['indice'] = 0
+        request.session['serie_atual'] = 0
+
+    indice = request.session['indice']
+    serie_atual = request.session['serie_atual']
+
+    # mostrar_descanso = request.session.get('mostrar_descanso', False)
+    # request.session['mostrar_descanso'] = False 
+    
+    if indice >= len(exercicios): #se o treino acabou
+            # Zera a sessão
+            request.session.pop('indice', None)
+            request.session.pop('serie_atual', None)
+            return redirect('home')
+    
+    exercicio_atual = exercicios[indice]
+    rel = rel_treino_exercicio.get(id_exercicio = exercicio_atual.id_exercicios)
+
+    if request.method == 'POST':
+        
+        if serie_atual+1 < rel.numero_series: #se as séries ainda não acabaram
+            serie_atual +=1
+            # mostrar_descanso = True  # pra ativar o popup
+            # request.session['mostrar_descanso'] = mostrar_descanso
+
+        else: #se a série acabou
+            # mostrar_descanso=False
+            if indice<len(exercicios): #vai para o próximo exercicio
+                indice+=1
+                serie_atual=0
+        
+        # Atualiza sessões
+        request.session['indice'] = indice
+        request.session['serie_atual'] = serie_atual
+        # # Recarrega view
+        # mostrar_descanso=False
+        return redirect('treinoia', pk=pk)
+
     context={
-        'treino': treino,
-        'exercicios': exercicios,
-        'rels': rel_treino_exercicio,
+        'rel': rel,
+        'exercicio_atual': exercicio_atual,
+        'serie_atual': serie_atual+1,
+        # 'mostrar_descanso':  mostrar_descanso,
     }
     return render(request,'treinoIA.html', context)
+
 
 def pagamento(request):
     return render(request, "pagamento.html")
